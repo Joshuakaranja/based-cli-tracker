d6889c22407125b312b965e563b440e2ff0d1471
Joshua Karanja <jokaranja26@gmail.com>
2025-08-28 09:56:44 +0300

    changes in cli.py

--- app/cli.py
+++ app/cli.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models import Base, User, Skill, PracticeSession
 
-DATABASE_URL = "sqlite:///tracker.db"  # Update with your database URL
+DATABASE_URL = "sqlite:///tracker.db"  
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
